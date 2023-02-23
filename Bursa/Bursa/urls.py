fd29f79859213766146c7bc787999cf555fe8aee
Ramis <Sir.ramis@mail.ru>
2023-02-23 16:20:55 +0300

    Создал urls файл в самом приложении и перенес функции, сделав приложение независимым от проекта

--- Bursa/Bursa/urls.py
+++ Bursa/Bursa/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from products.views import *
+from django.urls import path, include
 
-urlpatterns = [
+
+
+urlpatterns = {
     path('admin/', admin.site.urls),
-]
+    path('products/', include('products.urls'))
+}
+
+
