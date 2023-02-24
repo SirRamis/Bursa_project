615c96a52eadb325fcd2e960b995817c08e38824
Ramis <Sir.ramis@mail.ru>
2023-02-24 08:38:12 +0300

    Создал функцию архив

--- Bursa/Bursa/urls.py
+++ Bursa/Bursa/urls.py
@@ -13,15 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+#from django.conf.urls.static import static
 from django.contrib import admin
+
+from Bursa import settings
 from products.views import *
 from django.urls import path, include
 
 
 
-urlpatterns = {
+urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include('products.urls'))
-}
+    path('', include('products.urls')),
+   # path('about', include('about.urls')),
+]
 
-
+#if settings.DEBUG:
+    #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Bursa/products/urls.py
+++ Bursa/products/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+
+from django.urls import path, re_path
 from .views import *
 
+
+#def re_path(param, archive):
+ #   pass
+
+
 urlpatterns = [
-    path('', index),
-    path('cats/', categories),
+    path('', index), #name='home'),
+    path('cats/<int:catid>/', categories),
+    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
 ]
