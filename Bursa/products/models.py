2a31e6b54a5402f20198ec79f464f2264d191873
Ramis <Sir.ramis@mail.ru>
2023-09-12 20:14:58 +0300

    Added 12.09 funcshen

--- Bursa/products/models.py
+++ Bursa/products/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 # Create your models here.
 class Products(models.Model):
     title = models.CharField(max_length=255, verbose_name="Заголовок")
-    slug = models.Model.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+ #   slug = models.Model.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
     content = models.TextField(blank=True, verbose_name="Текст статьи")
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
@@ -27,7 +27,7 @@ class Products(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
-    slug = models.Model.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+ #   slug = models.Model.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
 
     def __str__(self):
         return self.name
